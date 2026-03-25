a5eba9d4d34be1dc7420494007913ca56ac41af8
arbabhussain-ct <88378935+ArbabHussain-CT@users.noreply.github.com>
2026-03-25 06:20:55 +0000

    improve dev flow by add dev container support

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Vite + React demo work</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is Lorem ipsum dolor sit. ewfewf efef {count}
